f132c6fcd5780713f61275415bca6b88eaddb388
Min Thant Kyaw <68982186+minthantkyaw28@users.noreply.github.com>
2023-10-17 01:58:13 +0630

    Update index.js

--- index.js
+++ index.js
@@ -35,21 +35,22 @@ const client = new MongoClient(process.env.MONGO_URL, {
     strict: true,
     deprecationErrors: true,
   },
+  useNewUrlParser: true, useUnifiedTopology: true,
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 //Getting collection from Database
 const clinica_db = client.db("clinica");
